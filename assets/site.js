65f70e410cf074699cafc786f561dc4c974bd2e8
Rob Kotz <rob.m.kotz@gmail.com>
2019-07-30 13:57:18 -1000

    setting up cookie for showing sale banner to email visitors

--- assets/site.js
+++ assets/site.js
@@ -2259,11 +2259,16 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="timer"
-  document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+  var timer = document.getElementById("timer")
+  if(timer) {
+   timer.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+  }
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("timer").innerHTML = "EXPIRED";
+    if(timer) {
+      timer.innerHTML = 'EXPIRED'; 
+    }
   }
 }, 1000);
