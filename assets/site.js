ff7d09073989dde53bc2febc82b9a5a0d9869d28
Syed AtiF Husain <atif@synchro.co>
2020-07-15 02:59:01 +0800

    Prepared for summer sale 2020

--- assets/site.js
+++ assets/site.js
@@ -2241,7 +2241,7 @@ $(document).ready(function() {
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 11 2019 23:59:59 UTC-0800").getTime();
+var countDownDate = new Date("Jul 17 2020 23:59:59 UTC-0700").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -2254,14 +2254,14 @@ var x = setInterval(function() {
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var hours = Math.floor(distance / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="timer"
   var timer = document.getElementById("timer")
   if(timer) {
-   timer.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+   timer.innerHTML = hours + "h " + minutes + "m " + seconds + "s";
   }
 
   // If the count down is finished, write some text
