5a504d2326988d53089f95c0fb01ffd2365cfac0
Syed AtiF Husain <atif@synchro.co>
2021-02-11 12:02:41 +0800

    Separate main and lp js/css

---
+++ src/synchro.js
@@ -0,0 +1,32 @@
+import './scss/synchro.scss';
+import LPEffects from './js/effects';
+import FaceBookPixel from './js/analytics/facebook_pixel';
+import Kissmetrics from './js/analytics/kissmetrics';
+import AnalyticsIntegration from './js/AnalyticsIntegration';
+import GradientEffect from './js/gradient_effect';
+import SVGLoader from './js/svg_loader';
+import Toggle from './js/toggle';
+import Tabs from './js/tabs';
+import Slider from './js/slider';
+import GoldCapBenefits from './js/goldCapBenefits';
+import FloatingPurchaseOptions from './js/floatingPurchaseOptions';
+import Experiments from './js/experiments';
+import { loadHeroImg } from './js/hero_image';
+
+console.log(`release_tag: ${process.env.RELEASE_TAG}`)
+
+const lpEffectsObj = new LPEffects();
+const fbPixelObj = new FaceBookPixel();
+const gradientObj = new GradientEffect();
+const svgObj = new SVGLoader();
+const toggle = new Toggle();
+const tabs = new Tabs();
+const analyticsObj = new AnalyticsIntegration();
+const goldCapBenefits = new GoldCapBenefits();
+const slider = Slider('.slides');
+
+//susanEffects();
+
+$(document).ready(() => {
+  gradientObj.startEffect();
+})
